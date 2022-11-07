36aecacfa024f4264b3197ea0b1831ae319fea1b
Himanshu23coder <gudushirodkar1997@gmail.com>
2022-11-07 15:47:45 +0530

    first commit

---
+++ src/GifPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Apps from "./components/Apps.css";
+
+const GifPage = () => {
+  return (
+    <div className="gif">
+      <div> </div>
+      <img
+        src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-32.gif"
+        alt="GIF"
+      />
+      <div> </div>
+    </div>
+  );
+};
+export default GifPage;
+
+// https://smashinghub.com/wp-content/uploads/2014/08/cool-loading-animated-gif-3.gif
+// https://i.pinimg.com/originals/a7/b0/15/a7b015d343ad801ad6da8c242dc6ae06.gif
